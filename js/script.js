7fbdc34dc443fc1845a4bd423f28ee6e87db508b
SurenYeremyan <email>
2023-05-06 10:44:51 +0400

    my project

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function Click1(){
+    var elem = document.getElementById("elem")
+    elem.src = "img/2.jpg"
+}
+function Click2(){
+    var elem = document.getElementById("elem")
+    elem.src = "img/3.jpg"
+}
+
+function Click3(){
+    var elem = document.getElementById("elem")
+    elem.src = "img/4.jpg"
+}
+
+function Click4(){
+    var elem = document.getElementById("elem")
+    elem.src = "img/5.jpg"
+}
+
+function Click5(){
+    var elem = document.getElementById("elem")
+    elem.src = "img/6.jpg"
+}
+
+function Click6(){
+    var elem = document.getElementById("elem")
+    elem.src = "img/7.jpg"
+}
+
+function Click7(){
+    var elem = document.getElementById("elem")
+    elem.src = "img/1.jpg"
+}
